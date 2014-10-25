2bd4c0de85890b649ee7d4fb2970deca56f464f0
John Skilling <johnskilling@gmail.com>
2014-10-25 02:31:12 +0100

    working first part lab

--- appointments.js
+++ appointments.js
@@ -1,32 +1,42 @@
-/**
- * Created by John on 21/10/2014.
- */
-    var Appointment = function (subject, description, date, time) {
-        this.subject = subject;
-        this.description = description;
-        this.datetime = new Date(date + "" + time);
-        this.completed = false;
-    };
-    var appointments =[];
+//var subject, description, enterdate, timestamp;
 
-    Appointment.prototype.isDue = function () {
-        var now = new Date();
-        if (now >= this.datetime) {
-            return true;
-        } else {
-            return false;
-        }
-    };
-    Appointment.prototype.whenDue = function () {
-            return this.datetime - new Date();
-        }
-    Appointment.prototype.toString = function () {
-            var s = this.subject + '\n' + this.description + '\n' + this.datetime.toString() + '\n';
-            if (this.completed) {
-                s += "Completed \n\n";
-            } else {
-                s += "Not Competed \n\n";
-            }
-            return s;
-        };
+var subject = prompt("Please enter the required subject"),
+    description = prompt("Please enter an appropriate description"),
+    enterdate = prompt("Please enter a required date Mon,DAY YEAR"),
+    timestamp = prompt("Please enter the required time 00:00");
 
+var appointments = [];
+
+//console.log(subject, description, enterdate, timestamp);
+var Appointment = function () {
+ this.subject = subject;
+ this.description = description;
+ this.date = enterdate;
+ this.time = timestamp;
+ this.datetime = new Date (enterdate + " " + timestamp);
+ this.completed = false;
+ };
+
+ Appointment.prototype.isDue = function () {
+ var now = new Date ();
+ if (this.datetime > now) {
+ return true;
+ } else {
+ return false;
+ }
+ };
+ Appointment.prototype.whenDue = function () {
+ return this.datetime - new Date ();
+ };
+ Appointment.prototype.toString = function () {
+ var s = this.subject + '\n' + this.description + '\n' +
+ this.datetime.toString () + '\n';
+ if (this.completed) {
+ s += "Completed\n\n";
+ } else {
+ s += "Not Completed\n\n";
+ }
+ return s;
+ };
+a = new Appointment(this.subject, this.description, this.datetime);  //test array properties of new object from constructer a.whenDue(); a.isDue(); and a.toString();
+appointments.push(a);
